e6f96481011ede743b37ddff0affce0026336379
Cassius <cassius.reynolds.dev@gmail.com>
2025-05-11 15:39:57 -0700

    changed hero title and the margin for it

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -33,9 +33,8 @@ const Landing = () => {
 
       <div className="mx-auto px-6 lg:px-8 absolute top-1/3 md:top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50">
         <div className=" w-screen lg:mx-0 mt-96 p-3 md:mt-64 xl:mt-64">
-          <h1 className="flex flex-wrap mt-64 sm:mt-40 justify-center gap-3 fade-in text-5xl font-semibold tracking-tight text-white md:text-6xl lg:text-7xl xl:text-8xl text-center w-screen md:w-auto">
-            <span>Move with Confidence,</span>{" "}
-            <span>Trust in Maximum Protection</span>
+          <h1 className="flex flex-wrap mt-[33rem] sm:mt-96 justify-center gap-3 fade-in text-5xl font-semibold tracking-tight text-white md:text-6xl lg:text-7xl xl:text-8xl text-center w-screen md:w-auto">
+            a team who cares and makes moving simple and stress free
           </h1>
         </div>
         <div className="fade-in mx-auto mt-16 grid max-w-7xl grid-cols-1 gap-6 sm:mt-20 lg:grid-cols-3 justify-items-center">
